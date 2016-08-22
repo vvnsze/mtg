0d704cbb4d91f87a4cc983ebfdf1eb1b8b9a3d0b
vvnsze <vvnsze@gmail.com>
2016-08-22 13:24:58 -0700

    working front page

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ var app = express();
 var mtg = require('mtgsdk');
 var bodyParser = require('body-parser');
 
+
+//middleware
+
+app.use('', express.static('../client'));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -12,20 +16,20 @@ app.use(function(req, res, next) {
 });
 
 
-//middleware
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(express.static('./client'));
+
 
 // app.get('/',function(req,res){
 //   res.send('Hello World');
 // });
 
-app.get('/',function(req,res){
+app.get('/card',function(req,res){
   mtg.card.find(Math.floor((Math.random() * 800) + 1))
   .then(result => {
-      console.log('___line 23___this is the name of the card' ,result.card.name); // "Black Lotus"
+      console.log('___line 23___this is the name of the card' ,result.card.name);
       console.log('+++line 23+++result.card' ,result.card);
       res.status(200).send(result.card);
   })
@@ -37,7 +41,6 @@ app.get('/',function(req,res){
 //setting server port
 app.set('port', process.env.PORT || 3000);
 
-app.listen( app.get('port')//,function(){
-  //console.log('+++line19 port is working');//
-//}//
-);
+app.listen( app.get('port'),function(){
+  console.log('+++line19 port is working');
+});
